39bf703020df01b1de1a5b69caeb0676a5ba75f7
Twisuki <Twisuki@outlook.com>
2025-08-04 23:33:26 +0800

    :sparkles: NavBar

--- client/docusaurus.config.ts
+++ client/docusaurus.config.ts
@@ -5,9 +5,9 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: '机器人学院学生手册',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.jpg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://twis.uk',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TZLakeStudio', // Usually your GitHub org/user name.
+  projectName: 'Application', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -32,8 +32,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -44,8 +44,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -55,8 +55,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -69,27 +69,78 @@ const config: Config = {
     ],
   ],
 
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
+      },
+    },
+  ],
+
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo.png',
     navbar: {
-      title: 'My Site',
+      title: '学生手册',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: '机器人学院学生手册Logo',
+        src: 'img/logo.png',
       },
       items: [
+        // 左
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          to: '/',
+          label: '主页',
           position: 'left',
-          label: 'Tutorial',
+          activeBasePath: '/',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          to: '/docs/study',
+          label: '入学',
+          position: 'left',
+          activeBaseRegex: 'docs/study/(next|v8)',
+        },
+        {
+          to: '/docs/study',
+          label: '生活',
+          position: 'left',
+          activeBaseRegex: 'docs/study/(next|v8)',
+        },
+        {
+          to: '/docs/study',
+          label: '学业',
+          position: 'left',
+          activeBaseRegex: 'docs/study/(next|v8)',
+        },
+        {
+          to: '/docs/study',
+          label: '毕业',
+          position: 'left',
+          activeBaseRegex: 'docs/study/(next|v8)',
+        },
+
+        // 右
+        {
+          to: '/docs/about',
+          label: '关于我们',
           position: 'right',
+          activeBaseRegex: 'docs/study/(next|v8)',
+        },
+        {
+          type: 'search',
+          position: 'right',
+        },
+        {
+          href: 'https://robotics.hnu.edu.cn',
+          position: 'right',
+          html: '<i class="fa-solid fa-house"></i>',
+        },
+        {
+          href: 'https://github.com/TZLakeStudio/Application',
+          position: 'right',
+          html: '<i class="fa-brands fa-github"></i>',
         },
       ],
     },
