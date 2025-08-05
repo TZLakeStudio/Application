187fd71e44fb30492b3eed28548a127fe814b2b8
Twisuki <Twisuki@outlook.com>
2025-08-05 17:31:20 +0800

    :sparkles: 主页

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,44 +1,36 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <div className="hero shadow--lw" style={{ height: '25rem' }}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
+        <h1 className="hero__title text--primary">机器人学院 <span className="text--success maple">学生手册</span></h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div>
+          <button className="button button--primary button--lg">开始阅读</button>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+          <button className="button button--secondary button--lg">加入我们</button>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title="首页 | 机器人学院学生手册"
+      description="一站式的学生信息获取站点">
+      <HomepageHeader/>
+      {/*<main>*/}
+      {/*  <HomepageFeatures/>*/}
+      {/*</main>*/}
     </Layout>
   );
 }
