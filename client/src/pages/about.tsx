9f7accd5d22244d9db2f9442a7130ddd665154e6
Twisuki <Twisuki@outlook.com>
2025-08-05 19:46:24 +0800

    :sparkles: about页

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,25 +1,34 @@
 import type {ReactNode} from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function AboutpageHeader() {
   return (
-    <div className="hero shadow--lw" style={{ height: '25rem' }}>
+    <div className="shadow--lw" style={{ height: '25rem' }}>
+      <div>
+        <br/><br/><br/><br/>
+      </div>
       <div className="container">
-        <h1 className="hero__title text--primary">机器人学院</h1>
-        <h1 className="hero__title text--success maple">桃子湖工作站</h1>
+        <h1 className="hero__title text--primary text--center">机器人学院</h1>
+        <h1 className="hero__title text--success maple text--center">桃子湖工作站</h1>
       </div>
     </div>
   );
 }
 
-export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
+function AboutpageMidder() {
+  return (
+    <div className="container">
+      本项目由机器人学院<a className="button button--link" href="https://github.com/TZLakeStudio">桃子湖工作站</a>维护
+    </div>
+  );
+}
+
+export default function About(): ReactNode {
   return (
     <Layout>
-      <HomepageHeader/>
+      <AboutpageHeader/>
+      <AboutpageMidder/>
     </Layout>
   );
 }
