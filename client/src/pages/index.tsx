a8a38290c5ae8b6d63675b3f48066cd0d81f2da5
Twisuki <Twisuki@outlook.com>
2025-08-17 18:16:22 +0800

    :lipstick: 主页hero响应式设计

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,19 +2,21 @@ import type {ReactNode} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from "@site/src/components/HomepageFeatures/HomepageFeatures";
+import styles from './index.module.css';
 
 
 function HomepageHeader(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <div className="hero shadow--lw" style={{ height: '25rem' }}>
-      <div className="container">
-        <h1 className="hero__title text--primary">人工智能与机器人学院 <span className="text--success maple">学生手册</span></h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div>
-          <a className="button button--primary button--lg" href="./docs/start">开始阅读</a>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <a className="button button--secondary button--lg" href="./about">加入我们</a>
+    <div className="container">
+      <div className={`hero shadow--lw ${styles.header}`}>
+        <h1 className={`text--primary ${styles.title}`}>
+            人工智能与机器人学院 <span className={`text--success maple ${styles.maple}`}>学生手册</span>
+        </h1>
+        <p className={styles.subtitle}>{siteConfig.tagline}</p>
+        <div className={styles.buttonGroup}>
+          <a className="button button--primary button--lg" href="/docs/start">开始阅读</a>
+          <a className="button button--secondary button--lg" href="/about">加入我们</a>
         </div>
       </div>
     </div>
