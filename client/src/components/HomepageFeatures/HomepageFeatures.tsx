273e690d52b8b94a435a4d04db0abb6411e70196
Kinoja <Kinoja.hnu.edu.cn>
2025-08-18 18:35:38 +0800

    Update HomepageFeatures component

--- client/src/components/HomepageFeatures/HomepageFeatures.tsx
+++ client/src/components/HomepageFeatures/HomepageFeatures.tsx
@@ -15,17 +15,17 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "生活",
-    description: <>学业有困惑?<br/>一键get官方咨询和学长学姐珍藏经验！</>,
+    description: <span>快乐生活每一天！<br/>囊括了娱乐饮食出行等各方面的生活密码！</span>,
     url: "/docs/life",
   },
   {
     title: "学业",
-    description: "学业描述文本.",
+    description: <span>学业有困惑?<br/>一键get官方咨询和学长学姐珍藏经验！</span>,
     url: "/docs/study",
   },
   {
     title: "毕业",
-    description: "毕业描述文本.",
+    description:  <span>生涯规划有困难？ <br/>优秀毕业生分享从保研、留学、工作全经验！</span>,
     url: "/docs/graduate",
   },
   {
